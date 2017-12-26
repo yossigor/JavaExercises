b4ca27c6f83cf3eabcd122614954e539250e855e
yossigor <yossigor@gmail.com>
2017-12-26 19:39:37 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+  public static Scanner reader = new Scanner (System.in); 
+  public static void main(String[] args)   
+  {
+  	System.out.println("Excersice with generic list");
+    double i = reader.nextDouble();
+    System.out.println(i);
+    
+  }
+}
