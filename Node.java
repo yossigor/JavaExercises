6615c680b10f4dc38625074d98348344ddf287cf
yossigor <yossigor@gmail.com>
2017-12-26 19:39:08 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,12 +1,45 @@
 import java.util.*;
 
-public class Main{
-  public static Scanner reader = new Scanner (System.in); 
-  public static void main(String[] args)   
-  {
-  	System.out.println("Excersice with generic list");
-    double i = reader.nextDouble();
-    System.out.println(i);
-    
-  }
-}
+public class Node<T>
+{
+    private T info;
+    private Node<T> next;
+    /* הפעולה בונה ומחזירה חוליה שהערך שלה הוא info ואין לה חוליה עוקבת **/
+    public Node(T info)
+    {
+        this.info = info;
+        this.next = null;
+    }
+    /*הפעולה בונה ומחזירה חוליה, שהערך שלה הוא info
+      והחוליה העוקבת לה היא החוליה next */
+    public Node(T info, Node<T> next)
+    {
+        this.info = info;
+        this.next = next;
+    }
+    /* הפעולה מחזירה את הערך של החוליה הנוכחית **/
+    public T GetInfo()
+    {
+        return info;
+    }
+    /* הפעולה מחזירה את החוליה העוקבת לחוליה הנוכחית **/
+    public Node<T> GetNext()
+    {
+        return next;
+    }
+    /* הפעולה קובעת את ערך החוליה הנוכחית להיות  info **/
+    public void SetInfo(T info)
+    {
+        this.info = info;
+    }
+    /* הפעולה קובעת את החוליה העוקבת לחוליה הנוכחית להיות החוליה next **/
+    public void SetNext(Node<T> next)
+    {
+        this.next = next;
+    }
+    /* הפעולה מחזירה מחרוזת המתארת את החוליה הנוכחית */
+    public  String ToString()
+    {
+        return this.info.toString();
+    }
+}//Class
