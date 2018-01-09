ee1582eedf52e6ebee7b54859211acb8448a786d
bat yam <bat yam@batyam-PC>
2018-01-09 18:32:48 +0200

    gun simmulator

--- Main.java
+++ Main.java
@@ -4,9 +4,36 @@ public class Main{
   public static Scanner reader = new Scanner (System.in); 
   public static void main(String[] args)   
   {
-  	System.out.println("Excersice with generic list");
-    double i = reader.nextDouble();
-    System.out.println(i);
-    
+	  int numberOfBullets=0;
+	  System.out.println("This App is a gun simulator:");
+	  System.out.print("Enter the number of bullets:");
+	  numberOfBullets=reader.nextInt();
+	  System.out.println("You asked for "+ numberOfBullets+" bullets"); 
+	  System.out.println("Entering bullets to stack...");
+	  Stack<Bullet> stack = new Stack<Bullet>();
+	  for (int i=0 ; i<numberOfBullets ; i++)
+	  {
+		  System.out.println("Entering bullet "+(i+1));
+		  Bullet bullet = new Bullet();
+		  stack.Push(bullet);
+	  }	
+	  int numberOfShoots=0;
+	  System.out.print("Enter the number of shoots:");
+	  numberOfShoots=reader.nextInt();
+	  for (int i=0 ; i<numberOfShoots ; i++)
+	  {
+		 shoot(stack);
+	  }
+  }
+  public static void shoot(Stack<Bullet> s)
+  {
+	  if (s.IsEmpty())
+	  {
+		  System.out.println("There is no bullets");
+	  }
+	  else {
+		  s.Pop();
+		  System.out.println("Boom!");
+	  }
   }
 }
--- Node.java
+++ Node.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Node<T>
 {
     private T info;
